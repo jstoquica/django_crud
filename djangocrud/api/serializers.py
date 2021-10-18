1c065155296ab1677608b43664b55dacab1a5a0d
jstoquica <jstoquica@gmail.com>
2021-10-18 17:56:11 -0300

    foi criado o modelo Movie

--- djangocrud/api/serializers.py
+++ djangocrud/api/serializers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import Movie
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class MovieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+        model = Movie
+        fields = ['id', 'title', 'desc', 'year']
